33f0432a4c3c6671b79346ee78d1e719d530f4e5
ImRishabhRawat <rishabhrawat757@gmail.com>
2022-12-07 22:59:45 +0530

    Upgraded animixBot

--- animixBot.py
+++ animixBot.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium. webdriver. chrome. options import Options
 import time
 
-#se=input("Enter a Topic you want to know : ")
-se= "One Piece"
+se=input("Enter a Topic you want to know : ")
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(chrome_options=options)
@@ -21,7 +21,7 @@ search = driver.find_element(By.ID,'q')
 search.send_keys(se)
 time.sleep(5)
 search.send_keys(Keys.ENTER)
-time.sleep(5)
+time.sleep(8)
 
 all_div = driver.find_element(By.XPATH,'//*[@id="result1"]/ul/li[1]')
 l=all_div.find_element(By.LINK_TEXT,se)
